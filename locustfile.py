421e8a5d9093560bdaed828b3f4fdd124557a68c
Andreas Neuber <github@andy-neuber.de>
2023-12-11 20:29:34 +0100

    Extend tests

--- locustfile.py
+++ locustfile.py
@@ -1,8 +1,25 @@
 from locust import HttpUser, task
+from termcolor import colored
 
 
 class HelloWorldUser(HttpUser):
     @task
-    def hello_world(self):
+    def form_1_information_about_yourself_page(self):
         self.client.get("/index.php?action=form1")
+
+    @task
+    def form_2_accommodation_booking_form_page(self):
         self.client.get("/index.php?action=form2")
+
+    @task
+    def form_3_credit_card_page(self):
+        self.client.get("/index.php?action=form3")
+
+    @task
+    def form_4_login_page(self):
+        self.client.get("/index.php?action=form4")
+
+        post_data = {'user': "admin", 'pw': 'pw1234'}
+        with self.client.post('/en/login', post_data, catch_response=True) as response:
+            if response.status_code != 200: print(
+                colored("Expected 200 but got {}".format(response.status_code), 'red'))
